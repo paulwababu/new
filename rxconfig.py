3486fa5d2481b6396748aa4a589a3d1c91cdcd82
paulwababu <44939805+paulwababu@users.noreply.github.com>
2023-12-07 10:56:35 +0300

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="zabunifund",
-    db_url="postgresql://postgres:lbo_dev@localhost/hello_reflex",
+    db_url="postgresql://postgres:lbo_dev@postgres/postgres",
 )
